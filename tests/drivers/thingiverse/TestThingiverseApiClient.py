671d0e2cfe6504b90505be89251e6f4523ce7f71
ChrisTerBeke <c.terbeke@ultimaker.com>
2020-03-31 09:51:04 +0200

    Remove pytest-qt, not working well

--- tests/drivers/thingiverse/TestThingiverseApiClient.py
+++ tests/drivers/thingiverse/TestThingiverseApiClient.py
@@ -10,7 +10,7 @@ API_CALL_TIMEOUT = 10000
 
 
 class TestThingiverseApiClient:
-    
+
     @pytest.fixture
     @surrogate("cura.CuraApplication.CuraApplication")
     def api_client(self, application):
@@ -21,23 +21,23 @@ class TestThingiverseApiClient:
     def test_getThingsFromCollectionQuery(self, api_client):
         query = api_client.getThingsFromCollectionQuery("my-collection")
         assert query == "collections/my-collection/things"
-        
+
     def test_getThingsBySearchQuery(self, api_client):
         query = api_client.getThingsBySearchQuery("cube")
         assert query == "search/cube/?sort=relevent"
-        
+
     def test_getThingsLikedByUserQuery(self, api_client):
         query = api_client.getThingsLikedByUserQuery()
         assert query == "users/None/likes"
-        
+
     def test_getThingsByUserQuery(self, api_client):
         query = api_client.getThingsByUserQuery()
         assert query == "users/None/things"
-        
+
     def test_getThingsMadeByUserQuery(self, api_client):
         query = api_client.getThingsMadeByUserQuery()
         assert query == "users/None/copies"
-        
+
     def test_getPopularThingsQuery(self, api_client):
         query = api_client.getPopularThingsQuery()
         assert query == "popular"
@@ -45,47 +45,7 @@ class TestThingiverseApiClient:
     def test_getFeaturedThingsQuery(self, api_client):
         query = api_client.getFeaturedThingsQuery()
         assert query == "featured"
-    
+
     def test_getNewestThingsQuery(self, api_client):
         query = api_client.getNewestThingsQuery()
         assert query == "newest"
-
-    def test_getCollections(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_finished:
-            api_client.getCollections(on_finished=on_finished)
-        assert len(on_finished.args) == 1
-
-    def test_getThings(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_finished:
-            api_client.getThings(query="popular", page=0, on_finished=on_finished)
-        assert len(on_finished.args) == 1
-
-    def test_getThings_error(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_failed:
-            api_client.getThings(query="wrong_query", page=0, on_finished=MagicMock(), on_failed=on_failed)
-        assert len(on_failed.args) == 1
-        
-    def test_getThing(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_finished:
-            api_client.getThing(thing_id=11551, on_finished=on_finished)
-        assert len(on_finished.args) == 1
-        
-    def test_getThing_error(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_failed:
-            api_client.getThing(thing_id=0, on_finished=MagicMock(), on_failed=on_failed)
-        assert len(on_failed.args) == 1
-
-    def test_getThingFiles(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_finished:
-            api_client.getThingFiles(thing_id=11551, on_finished=on_finished)
-        assert len(on_finished.args) == 1
-
-    def test_getThingFiles_error(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_failed:
-            api_client.getThingFiles(thing_id=0, on_finished=MagicMock(), on_failed=on_failed)
-        assert len(on_failed.args) == 1
-
-    def test_downloadThingFile(self, api_client, qtbot):
-        with qtbot.waitCallback(timeout=API_CALL_TIMEOUT) as on_finished:
-            api_client.downloadThingFile(file_id=1, file_name="", on_finished=on_finished)
-        assert len(on_finished.args) == 1
